a8c43ceaca13620a92590704b0976131c5f9b4f3
Mogeko <zhengjunyi@live.com>
2022-08-02 02:54:24 +0200

    test: remake the cache of swr after each test

--- lib/test-utils.ts
+++ lib/test-utils.ts
@@ -1,17 +1,33 @@
 import { render, renderHook } from "@testing-library/react";
 import type { RenderOptions, RenderHookOptions } from "@testing-library/react";
 import WrapProvider from "@/providers/providers";
+import { SWRConfig } from "swr";
+import React from "react";
 
 function renderWithProvider(ui: React.ReactElement, options?: RenderOptions) {
-  return render(ui, { wrapper: WrapProvider, ...options });
+  return render(ui, { wrapper: WrapProviderForTest, ...options });
 }
 
 function renderHookWithProvider<Result, Props>(
   render: (initialProps: Props) => Result,
   options?: RenderHookOptions<Props>
 ) {
-  return renderHook(render, { wrapper: WrapProvider, ...options });
+  return renderHook(render, { wrapper: WrapProviderForTest, ...options });
 }
+
+const WrapProviderForTest: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  return React.createElement(
+    WrapProvider,
+    null,
+    React.createElement(
+      SWRConfig,
+      { value: { provider: () => new Map() } },
+      children
+    )
+  );
+};
 
 export * from "@testing-library/react";
 export { renderHookWithProvider as renderHook };
