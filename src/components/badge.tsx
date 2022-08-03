7d1fb3c21f12827731cee8d07c8258c04da9b656
Mogeko <zhengjunyi@live.com>
2022-08-04 00:44:41 +0200

    feat: extract classNames as separate components

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -1,11 +1,12 @@
 import useMessage from "@/lib/hooks/useMessage";
+import classNames from "@/lib/utils/classNames";
 
 const Badge: React.FC<BadgeProps> = ({ badgeKey, className }) => {
   const { data } = useMessage();
   const count = data?.filter((m) => m.key === badgeKey).length;
   return count ? (
     <span
-      className={`badge ${className ?? ""}`}
+      className={classNames(className, "badge")}
       role="presentation"
       aria-label={`Here are ${count} new messages`}
     >
