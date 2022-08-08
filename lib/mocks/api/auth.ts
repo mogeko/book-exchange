2fde74e0a41a40d785720ba932290b33e994c2f3
Mogeko <zhengjunyi@live.com>
2022-08-08 21:44:17 +0200

    build: mock the API witch get the hmac key

--- lib/mocks/api/auth.ts
+++ lib/mocks/api/auth.ts
@@ -3,13 +3,24 @@ import { faker } from "@faker-js/faker";
 import { rest } from "msw";
 
 const authHandlers = [
+  rest.get("/api/auth/salt", (_, res, ctx) => {
+    return res(
+      ctx.json<Salt>({
+        salt: faker.random.alphaNumeric(16),
+      })
+    );
+  }),
   rest.post("/api/auth/login", async (req, res, ctx) => {
-    const { username, password } = await req.json<ReqType>();
+    const { username, password, salt } = await req.json<ReqType>();
+    const authID = `${randomNum({ min: 1000, max: 100000 })}`;
+    const authToken = faker.datatype.uuid();
 
     return res(
+      ctx.cookie("auth-id", authID),
+      ctx.cookie("auth-token", authToken),
       ctx.json<ResType>({
-        id: `${randomNum({ min: 1000, max: 100000 })}`,
-        token: faker.datatype.uuid(),
+        id: authID as `${number}`,
+        token: authToken,
       })
     );
   }),
@@ -18,11 +29,16 @@ const authHandlers = [
 interface ReqType {
   username: string;
   password: string;
+  salt: string;
 }
 
 interface ResType {
   id: `${number}`;
   token: string;
+}
+
+interface Salt {
+  salt: string;
 }
 
 export default authHandlers;
--- src/__tests__/login.test.tsx
+++ src/__tests__/login.test.tsx
@@ -3,6 +3,14 @@ import { render, screen } from "@testing-library/react";
 import fetcher from "@/lib/fetcher";
 
 describe("Authorize API", () => {
+  it("return a hmac salt", async () => {
+    const { salt } = await fetcher<Salt>("/api/auth/salt");
+
+    expect(salt).toBeDefined();
+
+    expect(salt).toMatchSnapshot();
+  });
+
   it("return an authorization token", async () => {
     const auth = await fetcher<ResType>("/api/auth/login", {
       method: "POST",
@@ -14,6 +22,14 @@ describe("Authorize API", () => {
         "Content-Type": "application/json",
       },
     });
+    const getCookie = (key: string) => {
+      const cookies = document.cookie.split("; ");
+
+      return cookies.find((c) => c.startsWith(key))?.split("=")[1];
+    };
+
+    expect(auth.id).toEqual(getCookie("auth-id"));
+    expect(auth.token).toEqual(getCookie("auth-token"));
 
     expect(auth).toMatchSnapshot();
   });
@@ -22,4 +38,8 @@ describe("Authorize API", () => {
 interface ResType {
   id: `${number}`;
   token: string;
+}
+
+interface Salt {
+  salt: string;
 }
