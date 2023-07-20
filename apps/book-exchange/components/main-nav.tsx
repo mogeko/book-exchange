5d55a419856e42434fbf5b3211eccf595be66126
Mogeko <zhengjunyi@live.com>
2023-07-20 23:35:48 +0200

    feat: implement RootLayout

---
+++ apps/book-exchange/components/main-nav.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { FaCircle } from "react-icons/fa";
+
+import { siteConfig } from "@/config/site";
+
+export const MainNav: React.FC = () => {
+  return (
+    <div className="flex sm:mr-6">
+      <Link href="/" className="flex mr-4 items-center space-x-2">
+        <FaCircle className="w-4 h-4 text-destructive" />
+        <span className="hidden align-middle font-bold sm:inline-block">
+          {siteConfig.title}
+        </span>
+      </Link>
+    </div>
+  );
+};
---
+++ apps/book-exchange/config/site.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: "Bookworm",
+  description: "A WebApplication for used book exchange",
+};
+
+export type SiteConfig = typeof siteConfig;
