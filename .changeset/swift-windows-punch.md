fc4f030eb1fd550cfb38ee41e7c69e5032cf59b7
Mogeko <zhengjunyi@live.com>
2023-07-23 13:59:03 +0200

    feat: migrate the relationship between `User` and `Book` to many-to-many

---
+++ .changeset/swift-windows-punch.md
@@ -0,0 +1,5 @@
+---
+"@mogeko/bookworm-db": minor
+---
+
+Migrate the relationship between `User` and `Book` to many-to-many.
--- packages/database/prisma/seed.ts
+++ packages/database/prisma/seed.ts
@@ -7,40 +7,57 @@ const prisma = new PrismaClient();
 async function seedYourDatabase() {
   faker.seed(1234); // To make sure we get the same data every time
 
-  const users = randomArrayWith(50, async () => {
-    const email = faker.internet.email();
-
-    return await prisma.user.upsert({
-      where: { email: email },
-      update: {},
-      create: {
-        email: email,
-        name: faker.internet.userName(),
-        avatar: faker.image.avatar(),
-        createdAt: faker.date.past(),
-        updatedAt: faker.date.recent(),
-        books: {
-          create: randomArrayWith(100, () => ({
-            title: faker.lorem.sentence(5),
-            author: faker.person.fullName(),
-            discription: faker.lorem.paragraph({ min: 5, max: 10 }),
-            cover: faker.image.urlLoremFlickr({ width: 1280, height: 1114 }),
-            isbn: faker.phone.number("978-#-##-######-#"),
-            createdAt: faker.date.past(),
-            updatedAt: faker.date.recent(),
-          })),
+  const books = await Promise.all(
+    randomArrayWith(100, async () => {
+      const isbn = faker.phone.number("978-#-##-######-#");
+      return await prisma.book.upsert({
+        where: { isbn: isbn },
+        update: {},
+        create: {
+          title: faker.lorem.sentence(5),
+          author: faker.person.fullName(),
+          discription: faker.lorem.paragraph({ min: 5, max: 10 }),
+          cover: faker.image.urlLoremFlickr({ width: 1280, height: 1114 }),
+          isbn: isbn,
+          createdAt: faker.date.past(),
+          updatedAt: faker.date.recent(),
         },
-      },
-    });
-  });
+      });
+    })
+  );
 
-  return await Promise.all(users);
+  return await Promise.all(
+    randomArrayWith(50, async () => {
+      const email = faker.internet.email();
+      return await prisma.user.upsert({
+        where: { email: email },
+        update: {},
+        create: {
+          email: email,
+          name: faker.internet.userName(),
+          avatar: faker.image.avatar(),
+          createdAt: faker.date.past(),
+          updatedAt: faker.date.recent(),
+          books: {
+            create: arrayElements(books, { min: 0, max: 20 }).map((book) => {
+              return {
+                book: { connect: { id: book.id } },
+              };
+            }),
+          },
+        },
+      });
+    })
+  );
 }
 
 /** Helper function to generate an array of random values */
 function randomArrayWith<T>(length: number, fn: () => T) {
   return Array.from({ length: faker.number.int(length) }).map(fn);
 }
+
+/** Helper function to generate an array of random values */
+const arrayElements = faker.helpers.arrayElements;
 
 // Run the seed function
 (async () => {
