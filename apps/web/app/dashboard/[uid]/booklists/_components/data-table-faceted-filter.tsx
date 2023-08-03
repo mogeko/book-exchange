11e09951f7ea880146db7f9f97d8187518ebcc3a
Mogeko <zhengjunyi@live.com>
2023-08-03 21:33:12 +0200

    feat: add faceted filter to the table

---
+++ apps/web/app/dashboard/[uid]/booklists/_components/data-table-faceted-filter.tsx
@@ -0,0 +1,141 @@
+import type { Column } from "@tanstack/react-table";
+import { RxCheck, RxPlusCircled } from "react-icons/rx";
+
+import { cn } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Separator } from "@/components/ui/separator";
+
+export function DataTableFacetedFilter<TData, TValue>(props: {
+  column?: Column<TData, TValue>;
+  title?: string;
+  options: {
+    label: string;
+    value: string;
+    icon?: React.ComponentType<{ className?: string }>;
+  }[];
+}) {
+  const { column, title, options } = props;
+  const facets = column?.getFacetedUniqueValues();
+  const selectedValues = new Set(column?.getFilterValue() as string[]);
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="outline" size="sm" className="h-8 border-dashed">
+          <RxPlusCircled className="mr-2 h-4 w-4" />
+          <span>{title}</span>
+          {selectedValues?.size > 0 && (
+            <>
+              <Separator orientation="vertical" className="mx-2 h-4" />
+              <Badge
+                variant="secondary"
+                className="rounded-sm px-1 font-normal lg:hidden"
+              >
+                {selectedValues.size}
+              </Badge>
+              <div className="hidden space-x-1 lg:flex">
+                {selectedValues.size > 2 ? (
+                  <Badge
+                    variant="secondary"
+                    className="rounded-sm px-1 font-normal"
+                  >
+                    {selectedValues.size} selected
+                  </Badge>
+                ) : (
+                  options
+                    .filter(({ value }) => selectedValues.has(value))
+                    .map(({ value, label }) => (
+                      <Badge
+                        key={`faceted-filter-badge-${value}`}
+                        variant="secondary"
+                        className="rounded-sm px-1 font-normal"
+                      >
+                        {label}
+                      </Badge>
+                    ))
+                )}
+              </div>
+            </>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0" align="start">
+        <Command>
+          <CommandInput placeholder={title} />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup>
+              {options.map(({ value, label, icon: OptionIcon }) => {
+                const isSelected = selectedValues.has(value);
+                return (
+                  <CommandItem
+                    key={`faceted-filter-cmd-${value}`}
+                    onSelect={() => {
+                      if (isSelected) {
+                        selectedValues.delete(value);
+                      } else {
+                        selectedValues.add(value);
+                      }
+                      const filterValues = Array.from(selectedValues);
+                      column?.setFilterValue(
+                        filterValues.length ? filterValues : undefined
+                      );
+                    }}
+                  >
+                    <div
+                      className={cn(
+                        "mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
+                        isSelected
+                          ? "bg-primary text-primary-foreground"
+                          : "opacity-50 [&_svg]:invisible"
+                      )}
+                    >
+                      <RxCheck className="h-4 w-4" />
+                    </div>
+                    {OptionIcon && (
+                      <OptionIcon className="mr-2 h-4 w-4 text-muted-foreground" />
+                    )}
+                    <span>{label}</span>
+                    {facets?.get(value) && (
+                      <span className="ml-auto flex h-4 w-4 items-center justify-center font-mono text-xs">
+                        {facets.get(value)}
+                      </span>
+                    )}
+                  </CommandItem>
+                );
+              })}
+            </CommandGroup>
+            {selectedValues.size > 0 && (
+              <>
+                <CommandSeparator />
+                <CommandGroup>
+                  <CommandItem
+                    onSelect={() => column?.setFilterValue(undefined)}
+                    className="justify-center text-center"
+                  >
+                    Clear filters
+                  </CommandItem>
+                </CommandGroup>
+              </>
+            )}
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
--- apps/web/app/dashboard/[uid]/booklists/_components/data-table-toolbar.tsx
+++ apps/web/app/dashboard/[uid]/booklists/_components/data-table-toolbar.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import type { Table } from "@tanstack/react-table";
+import { RxCross2 } from "react-icons/rx";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { DataTableFacetedFilter } from "@/app/dashboard/[uid]/booklists/_components/data-table-faceted-filter";
+import { priorities } from "@/app/dashboard/[uid]/booklists/_lib/priorities";
+import { statuses } from "@/app/dashboard/[uid]/booklists/_lib/statues";
 
 export function DataTableToolbar<TData>({ table }: { table: Table<TData> }) {
-  const isFiltered = table.getState().columnFilters.length > 0;
-
   return (
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
@@ -19,6 +21,30 @@ export function DataTableToolbar<TData>({ table }: { table: Table<TData> }) {
           }
           className="h-8 w-[150px] lg:w-[250px]"
         />
+        {table.getColumn("status") && (
+          <DataTableFacetedFilter
+            column={table.getColumn("status")}
+            title="Status"
+            options={statuses}
+          />
+        )}
+        {table.getColumn("priority") && (
+          <DataTableFacetedFilter
+            column={table.getColumn("priority")}
+            title="Priority"
+            options={priorities}
+          />
+        )}
+        {table.getState().columnFilters.length > 0 && (
+          <Button
+            variant="ghost"
+            onClick={() => table.resetColumnFilters()}
+            className="h-8 px-2 lg:px-3"
+          >
+            Reset
+            <RxCross2 className="ml-2 h-4 w-4" />
+          </Button>
+        )}
       </div>
     </div>
   );
