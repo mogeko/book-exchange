e223f737df4363bb65366f57fdd4a0ca0b041101
Mogeko <zhengjunyi@live.com>
2023-08-05 00:18:09 +0200

    feat: search for the author's name and ISBN number

--- apps/web/actions/search.ts
+++ apps/web/actions/search.ts
@@ -4,7 +4,18 @@ import { prisma } from "@/lib/database";
 
 export async function search(query: string) {
   return await prisma.book.findMany({
-    where: { title: { contains: query } },
+    where: {
+      OR: [
+        { title: { contains: query } },
+        { authors: { some: { name: { contains: query } } } },
+        { isbn: { contains: query } },
+      ],
+    },
     select: { id: true, title: true },
+    orderBy: {
+      owners: { _count: "desc" },
+    },
+    take: 10,
+    skip: 0,
   });
 }
