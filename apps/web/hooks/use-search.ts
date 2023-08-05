f1b7c4b5aa0098db746db7178fbd0f2476a7fa9e
Mogeko <zhengjunyi@live.com>
2023-08-05 05:19:15 +0200

    feat: handling errors, when the query fails

--- apps/web/hooks/use-search.ts
+++ apps/web/hooks/use-search.ts
@@ -4,20 +4,26 @@ import { search } from "@/actions/search";
 import useSWR from "swr";
 
 import { useDebounce } from "@/hooks/use-debounce";
+import { useToast } from "@/hooks/use-toast";
 
 export function useSearch(value: string) {
   const debouncedValue = useDebounce(value, 500);
-  const { data, error, isLoading } = useSWR(
-    () => (value.length > 0 ? debouncedValue : null),
-    (value: string) => search(value),
-    { keepPreviousData: true }
-  );
+  const { toast } = useToast();
 
-  if (value.length <= 0) {
-    return { isLoading: false, data: null, error: null };
-  } else {
-    return { isLoading, data, error };
-  }
+  return useSWR(
+    () => (value.length ? debouncedValue : null),
+    (value: string) => search(value),
+    {
+      keepPreviousData: !!value.length,
+      onError: (error) => {
+        toast({
+          variant: "destructive",
+          title: "Oooooops! Something went wrong.",
+          description: error.message,
+        });
+      },
+    }
+  );
 }
 
 export type Book = Awaited<ReturnType<typeof search>>[number];
